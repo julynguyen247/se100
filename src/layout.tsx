16d9785d29fbee52ca948c6d681206abc2eefdc8
julynguyen247 <khoi.nguyen@alp-management.com>
2026-01-07 21:31:16 +0700

    set appointment

--- src/layout.tsx
+++ src/layout.tsx
@@ -1,13 +1,9 @@
 import { Outlet } from "react-router-dom";
-import AppHeader from "./components/layout/app.header";
-import AppFooter from "./components/layout/app.footer";
 
 const Layout = () => {
   return (
     <>
-      <AppHeader />
       <Outlet />
-      <AppFooter />
     </>
   );
 };
